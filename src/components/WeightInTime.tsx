28df8fa8b0751f2e985b03f0177a195f71fbc963
illispi <44943503+illispi@users.noreply.github.com>
2024-01-26 07:03:24 +0200

    types for trpc props

---
+++ src/components/WeightInTime.tsx
@@ -0,0 +1,23 @@
+import { type Component } from "solid-js";
+import type { inferRouterOutputs } from '@trpc/server';
+import type { AppRouter } from '../server/router';
+ 
+type RouterOutput = inferRouterOutputs<AppRouter>;
+
+type WeightsDates = RouterOutput['readWeights'];
+
+const WeightInTime: Component<{ weeks: number; weightsDates: WeightsDates; text: string }> = (
+  props,
+) => {
+  const lostOrGained = (weeks: number, weightsDates: WeightsDates) => {
+    const startDate = weightsDates[weightsDates.length - 1];
+    let date = new Date();
+    date.setDate(date.getDate() - 7);
+
+    return date.toISOString();
+  };
+
+  return <h2>{lostOrGained(props.weeks, props.weightsDates)}</h2>;
+};
+
+export default WeightInTime;
--- src/components/pages/IndexPage.tsx
+++ src/components/pages/IndexPage.tsx
@@ -1,16 +1,27 @@
 import { QueryClientProvider } from "@tanstack/solid-query";
 import { For, type Component } from "solid-js";
-import { queryClient, trpc } from "../utils/trpcClient";
-import WeightIcon from "./WeightIcon";
-import ExerciseIcon from "./ExerciseIcon";
+import { queryClient, trpc } from "../../utils/trpcClient";
+import WeightInTime from "../WeightInTime";
 
 const IndexSub: Component<{}> = (props) => {
   const weightsDates = trpc.readWeights.useQuery();
   return (
     <div class="flex min-h-screen w-full flex-col items-center justify-start lg:justify-center">
       <div class="flex w-full flex-col items-center justify-center">
-        <WeightIcon />
-        <ExerciseIcon />
+        {/* Weight banner */}
+        <div class="flex w-full max-w-screen-lg flex-col lg:flex-row">
+          <div class="flex flex-1 flex-col items-center justify-center">
+            <h1>Weight tracker</h1>
+            <div>Lost last week</div>
+            <div>Lost last month</div>
+          </div>
+          <div class="flex-1">Chart total</div>
+        </div>
+        {/* Exercise banner */}
+        <div>
+          <div>Exercise icon</div>
+        </div>
+        <WeightInTime text="last week" weeks={1} weightsDates={weightsDates.data} />
         <For each={weightsDates.data}>
           {(item) => (
             <div>
