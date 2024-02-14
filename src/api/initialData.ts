d62eaac386f90d364e221c5cd1d6501ec5367607
illispi <44943503+illispi@users.noreply.github.com>
2024-02-14 10:51:01 +0200

    tanstack router

--- src/api/initialData.ts
+++
@@ -1,7 +0,0 @@
-export const getGreeting = async () => {
-  const stats = await fetch("http://localhost:4321/trpc/greeting").then((res) =>
-    res.json()
-  );
-  console.log(stats)
-  return stats;
-};
--- src/server/router.ts
+++
@@ -1,9 +0,0 @@
-import { readWeights } from "./routers/readWeights";
-import { apiProcedure, router } from "./utils";
-
-export const appRouter = router({
-  greeting: apiProcedure.query(() => "hello tRPC v10!"),
-  readWeights,
-});
-
-export type AppRouter = typeof appRouter;
