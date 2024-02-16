3fa68f23c779bc8534d969b23872ab3b75d56a2d
illispi <44943503+illispi@users.noreply.github.com>
2024-02-16 14:41:05 +0200

    VT api wip

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,5 +1,6 @@
-import { createLazyFileRoute } from "@tanstack/react-router";
+import { Link, createLazyFileRoute, useNavigate } from "@tanstack/react-router";
 import { trpc } from "../client/client";
+import { flushSync } from "react-dom";
 
 export const Route = createLazyFileRoute("/")({
 	component: Index,
@@ -7,10 +8,24 @@ export const Route = createLazyFileRoute("/")({
 
 function Index() {
 	const weights = trpc.readWeights.useQuery();
+	const navigate = useNavigate();
 
 	return (
 		<>
 			<div className="p-2">
+				<Link
+					to="/about"
+					onClick={(ev) => {
+						ev.preventDefault();
+						document.startViewTransition(() => {
+							flushSync(() => {
+								navigate({ to: "/about" });
+							});
+						});
+					}}
+				>
+					about
+				</Link>
 				{weights.data?.map((e, i) => (
 					<div key={`weight_date_${i}`}>
 						<p>{e.date}</p>
