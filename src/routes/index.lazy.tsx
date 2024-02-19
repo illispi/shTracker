f9046bce1227246f248826e23a2872f00655f9a4
illispi <44943503+illispi@users.noreply.github.com>
2024-02-19 11:42:58 +0200

    orderby, concurrently, and base chart

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,5 +1,7 @@
 import { createLazyFileRoute } from "@tanstack/react-router";
 import { trpc } from "../client/client";
+import { Line } from "react-chartjs-2";
+import "chart.js/auto";
 
 export const Route = createLazyFileRoute("/")({
 	component: Index,
@@ -8,13 +10,25 @@ export const Route = createLazyFileRoute("/")({
 function Index() {
 	const weights = trpc.readWeights.useQuery();
 
+	const data = {
+		labels: weights.data?.map((e) => new Date(e.date).toDateString()),
+		datasets: [
+			{
+				label: "Weight",
+				data: weights.data?.map((e) => e.weight),
+			},
+		],
+	};
+
 	return (
 		<>
-			<div className="p-2">
+			<div className="p-2 w-full flex flex-col justify-center items-center">
+				<Line data={data} />
 				{weights.data?.map((e) => (
 					<div key={e.id}>
 						<p>{e.date}</p>
 						<p>{e.weight}</p>
+						<p>{e.id}</p>
 					</div>
 				))}
 			</div>
--- src/server/routes/weightRoutes.ts
+++ src/server/routes/weightRoutes.ts
@@ -4,6 +4,7 @@ export const readWeights = apiProcedure.query(async ({ ctx }) => {
 	const weights = await ctx.db
 		.selectFrom("weight")
 		.selectAll("weight")
+		.orderBy("id asc")
 		.execute();
 
 	return weights;
